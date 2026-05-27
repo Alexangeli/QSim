8220c303444189025a82a850dd585b2a28713312
Alexangels05 <angeli.2209267@studenti.uniroma1.it>
2026-05-27 18:27:58 +0200

    add thread.h with thread_arg_t structure and function prototypes

---
+++ include/thread.h
@@ -0,0 +1,49 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include "matrix.h"
+#include "complex.h"
+#include "parser.h"
+
+/*
+ * Structure containing arguments for a matrix multiplication thread.
+ *
+ * a:      first matrix 
+ * b:      second matrix 
+ * result: result matrix, allocated by the thread 
+ */
+typedef struct {
+    matrix_t *a;      /* first matrix */
+    matrix_t *b;      /* second matrix */
+    matrix_t *result; /* result matrix */
+} thread_arg_t;
+
+/*
+ * Thread function that multiplies two matrices.
+ *
+ * Input:  arg - pointer to thread_arg_t 
+ * Output: NULL
+ */
+void *thread_matrix_mul(void *arg);
+
+/*
+ * Computes the final matrix by multiplying all gates in parallel.
+ *
+ * Input:  c           - circuit 
+ *         n_threads - number of threads 
+ * Output: pointer to the final matrix 
+ */
+matrix_t *compute_unitary(circuit_t *c, int num_threads);
+
+/*
+ * Applies the final matrix to the initial state vector.
+ *
+ * Input:  m     - final matrix 
+ *         state - initial state vector 
+ *         n     - number of qubits 
+ * Output: pointer to the final state vector 
+ */
+complex_t *apply_unitary(matrix_t *m, complex_t *state, int n);
+
+
+# endif
