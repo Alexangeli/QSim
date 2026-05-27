e1ee3cf86c462777c855bf2d4f4c946233d623d5
Alexangels05 <angeli.2209267@studenti.uniroma1.it>
2026-05-27 18:07:01 +0200

    feat: modified main.c to test the parser (successfully)

--- src/main.c
+++ src/main.c
@@ -32,27 +32,31 @@ int main(int argc, char *argv[]) {
     init_state_t *s = parse_init_state(state_file);
     if (!s) { fprintf(stderr, "Errore parsing stato iniziale\n"); exit(EXIT_FAILURE); }
 
-    printf("qubits: %d\n", s->n);
-    printf("state vector (%d elements):\n", 1 << s->n);
-    for (int i = 0; i < (1 << s->n); i++)
-        printf("  [%d] %f + i%f\n", i, s->state[i].real, s->state[i].imag);
-
     /* parse circuit */
     circuit_t *c = parse_circuit(circuit_file, s->n);
     if (!c) { fprintf(stderr, "Errore parsing circuito\n"); exit(EXIT_FAILURE); }
 
+
+    printf("qubits: %d\n", s->n);
+    printf("state vector (%d elements):\n", 1 << s->n);
+    /*for (int i = 0; i < (1 << s->n); i++)
+        printf("  [%d] %f + i%f\n", i, s->state[i].real, s->state[i].imag);*/
+
     printf("\ngates defined: %d\n", c->num_gates);
     for (int i = 0; i < c->num_gates; i++) {
-    printf("  gate[%d]: %s\n", i, c->gates[i]->name);
-    int sz = c->gates[i]->matrix->size;
-    for (int r = 0; r < sz; r++)
+        printf("  gate[%d]: %s, size %dx%d\n", i,
+               c->gates[i]->name,
+               c->gates[i]->matrix->size,
+               c->gates[i]->matrix->size);
+
+    /*for (int r = 0; r < sz; r++)
         for (int cl = 0; cl < sz; cl++)
             printf("    [%d][%d] = %f + i%f\n", r, cl,
                    c->gates[i]->matrix->data[r][cl].real,
-                   c->gates[i]->matrix->data[r][cl].imag);
+                   c->gates[i]->matrix->data[r][cl].imag);*/
 }
 
-    printf("\ncircuit length: %d\n", c->circ_len);
+    printf("circuit length: %d\n", c->circ_len);
     for (int i = 0; i < c->circ_len; i++)
         printf("  step[%d]: %s\n", i, c->gates[c->circ[i]]->name);
 
